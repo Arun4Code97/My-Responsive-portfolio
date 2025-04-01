9e0ba9458a2d5e164adc983499a23f9fdd02c2ae
arun4code97 <arunbairav12@gmail.com>
2025-04-01 18:29:01 +0530

    mediaQueryForMobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,65 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
+/*=============== MENU SHOW ===============*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*=============== MENU Hidden ===============*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR TO HEADER ===============*/
-
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the scroll is greater than 50 of the viewport, add class scroll-header
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage= document.getElementById('contact-message')
 
+const sendEmail = (e) =>{
+    e.preventDefault()
 
-/*=============== SHOW SCROLL UP ===============*/ 
+    //serviceID- templateID - #form - public key
+    emailjs.sendForm('service_r07sno9', 'template_chx3o8v', 
+        '#contact-form', 'kO5hJgLNB-haToWgv')
+    .then(() =>{
+        //Show sent message
+        contactMessage.textContent = 'Message sent successfully'
+    }, () =>{
+        //Show error message
+        contactMessage.textContent = 'Error sending message'
+    })
+    
+}
+
+contactForm.addEventListener('submit', sendEmail)
+      /*=============== SHOW SCROLL UP ===============*/ 
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
