e70aee18c4d37b16fcbec52554a40170e4d80f81
arun4code97 <arunbairav12@gmail.com>
2025-04-11 20:20:18 +0530

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,7 @@ const navLink = document.querySelectorAll('.nav__link')
 
 const linkAction = () =>{
     const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
+    // When click on each nav__link, remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
@@ -50,19 +50,74 @@ const sendEmail = (e) =>{
         '#contact-form', 'kO5hJgLNB-haToWgv')
     .then(() =>{
         //Show sent message
-        contactMessage.textContent = 'Message sent successfully'
+        contactMessage.innerHTML = 'Message sent successfully &#9989;'
+        
+        //Remove message after 5 seconds
+        setTimeout(() =>{
+            contactMessage.textContent = ''
+        }, 4000)
+
+        //Reset form or clear input fields
+            contactForm.reset()
+    
     }, () =>{
         //Show error message
-        contactMessage.textContent = 'Error sending message'
+        contactMessage.innerHTML = 'Error sending message &#10060;'
     })
     
 }
 
 contactForm.addEventListener('submit', sendEmail)
-      /*=============== SHOW SCROLL UP ===============*/ 
 
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58, /*uses nav__menu height*/
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true // Animation repeat
+})
+
+sr.reveal(`.home__data, .home__social, .contact__container, .footer__container`)
+sr.reveal(`.home__image`, {origin: 'bottom'} )
+sr.reveal(`.about__data, .skills__data`,{origin:'left'})
+sr.reveal(`.about__image, .skills__content `,{origin:'right'})
+sr.reveal(`.services__card, .projects__card`, {interval: 200})
+
+
+
+
+
+
+
+
